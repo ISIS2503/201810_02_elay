5b21a93286504151ba2ec72a77d767bd7ce6ae56
WILLIAM STIVEN DUARTE <ws.duarte@sis.virtual.uniandes.edu.co>
2018-03-23 16:20:06 -0500

    Programa Speed funcioando con servicios

--- Entrega 1/Avances programa p/ProgramaP/src/progama/Client.java
+++ Entrega 1/Avances programa p/ProgramaP/src/progama/Client.java
@@ -62,6 +62,10 @@ public class Client extends Application{
 		for(int i = 0; i < 4; i++) lista.add(new LinkedList<Countent>());
 	}
 	
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+	
 	public Client(Interfaz i) {
 		this.interfaz = i;
 		imprimir("propietario", prueba);
