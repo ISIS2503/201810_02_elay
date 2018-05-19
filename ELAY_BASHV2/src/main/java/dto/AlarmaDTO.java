62a60c154efb792448a85ec27ff50cd3cbaf96f7
juandiegotm <jd.trujillom@uniandes.edu.co>
2018-05-19 12:08:01 -0500

    Anadida informacion del inmueble a las alarmas

--- ELAY_BASHV2/src/main/java/dto/AlarmaDTO.java
+++ ELAY_BASHV2/src/main/java/dto/AlarmaDTO.java
@@ -10,23 +10,47 @@ public class AlarmaDTO {
     public String timestamp;
     public int alertaId;
     public String mensajeAlerta;
+    public int aptoAlarma;
+    public int torreoAlarma;
 
     public AlarmaDTO() {
     }
 
-    public AlarmaDTO(String id, String timestamp, int alertaId, String mensajeAlerta) {
+    public AlarmaDTO(String id, String timestamp, int alertaId, String mensajeAlerta, int aptoAlarma, int torreoAlarma) {
         this.id = id;
         this.timestamp = timestamp;
         this.alertaId = alertaId;
         this.mensajeAlerta = mensajeAlerta;
+        this.aptoAlarma = aptoAlarma;
+        this.torreoAlarma = torreoAlarma;
     }
-    
+
     public AlarmaDTO(Alarma entity){
         this.id = entity.getId();
         this.alertaId = entity.getAlertaId();
         this.mensajeAlerta = entity.getMensajeAlerta();
         this.timestamp = entity.getTimestamp();
+        this.aptoAlarma = entity.getAptoAlarma();
+        this.torreoAlarma = entity.getTorreAlarma();
     }
+
+    public int getAptoAlarma() {
+        return aptoAlarma;
+    }
+
+    public void setAptoAlarma(int aptoAlarma) {
+        this.aptoAlarma = aptoAlarma;
+    }
+
+    public int getTorreoAlarma() {
+        return torreoAlarma;
+    }
+
+    public void setTorreoAlarma(int torreoAlarma) {
+        this.torreoAlarma = torreoAlarma;
+    }
+    
+    
 
     public String getId() {
         return id;
@@ -66,6 +90,8 @@ public class AlarmaDTO {
         alarma.setAlertaId(alertaId);
         alarma.setMensajeAlerta(mensajeAlerta);
         alarma.setTimestamp(timestamp);
+        alarma.setAptoAlarma(aptoAlarma);
+        alarma.setTorreAlarma(torreoAlarma);
         
         return alarma;
     }
